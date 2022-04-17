ac5034a919fd1926fe1d23dd072650a7ffc941a5
prabesh12 <prabesholi8848@gmail.com>
2022-04-17 18:47:18 +0545

    used-productList-from-store-to-home-scree

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,24 +3,33 @@ import Product from "../components/Product";
 import axios from "axios";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../action/ProductAction";
 const HomeScreen = () => {
-  const [product, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  // const [product, setProduct] = useState([])
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState()
+  const dispatch = useDispatch();
+ const productList = useSelector((state)=>state.productList)
+ const {loading, error, products} = productList;
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get("/api/products");
-        setLoading(false);
-        setProducts(data);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
-    };
-    fetchData();
+    
+    dispatch(listProducts())
+    // const fetchData = async () => {
+    //   try {
+    //     setLoading(true);
+    //     const { data } = await axios.get("/api/products");
+    //     setLoading(false);
+    //     setProducts(data);
+    //   } catch (error) {
+    //     setError(error.message);
+    //     setLoading(false);
+    //   }
+    // };
+    // fetchData();
+
+
   }, []);
 
   return (
@@ -31,8 +40,7 @@ const HomeScreen = () => {
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
         <div className="row center">
-          {product.map((product) => {
-            console.log(product);
+          {products.map((product) => {
             return <Product key={product._id} products={product}></Product>;
           })}
         </div>
