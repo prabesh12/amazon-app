b0892cbd84ccf14964d5b0da8be2363bc4c6e9cc
prabesh12 <prabesholi8848@gmail.com>
2022-04-17 17:58:23 +0545

    defined-product-constants

---
+++ frontend/src/constants/ProductConstants.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const PRODUCT_LIST_REQUEST ="RODUCT_LIST_REQUEST";
+export const PRODUCT_LIST_SUCCESS ="RODUCT_LIST_SUCCESS";
+export const PRODUCT_LIST_FAIL ="RODUCT_LIST_FAIL";
+
+
+
+const ProductConstants = () => {
+  return (
+    <div>ProductConstants</div>
+  )
+}
+
+export default ProductConstants
