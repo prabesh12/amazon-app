a909c8162b518f6b89cb2278effe52470bc6f9f8
prabesh12 <prabesholi8848@gmail.com>
2022-04-19 14:03:57 +0545

    created-api-for-detailPage

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,15 @@ import data from "./datalist.js";
 const app = express();
 
 app.get("/api/products", (req, res)=>{
+    const product = data.product.find((x)=> x._id ===req.params.id);
+    if(product){
+    res.send(data.product);
+    }
+    else
+    res.status(404).send({message:"product not found"});
+})
+
+app.get("/api/products:id", (req, res)=>{
     res.send(data.product);
 })
 
